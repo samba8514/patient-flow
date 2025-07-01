9d5cf49bf3f9140249a92efd93fc9cd4f31c8d94
samba <samba8514@gmail.com>
2025-07-01 22:54:05 +0200

    minor fix for loading env variables

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,11 @@ from models import db, User, Task, Patient
 from utils import admin_required
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback-secret')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pfuser:pfpass123@localhost/patientflow'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
